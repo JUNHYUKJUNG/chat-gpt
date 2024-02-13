9c08f6a26119233277a9b9938254a73d0892634f
JUNHYUK JUNG <kuyhnuj@outlook.com>
2024-02-14 03:30:58 +0900

    Delete Text-Center

--- src/components/ChatSide.jsx
+++ src/components/ChatSide.jsx
@@ -3,7 +3,7 @@ import ChatSideCard from "./ChatSideCard";
 const ChatSide = ({ chatList }) => {
   return (
     <ul className="bg-gray-50 w-52 p-4">
-      <div className="text-center text-lg">과거 채팅 기록</div>
+      <div className="text-lg">과거 채팅 기록</div>
       {chatList?.map((v, i) => (
         <ChatSideCard key={i} question={v.question} answer={v.answer} />
       ))}
